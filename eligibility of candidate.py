011e36e20c32453493bad7f85fdd402f3cab3f03
Yash Shukla <119918899+yash7399@users.noreply.github.com>
2022-12-08 22:01:40 +0530

    Add files via upload

---
+++ eligibility of candidate.py
@@ -0,0 +1,6 @@
+age=int(input('enter your age :'))
+height=int(input('enter your height in cm :'))
+if age>=17 and age<=21 and height>=172 and height <= 190:
+    print(' You are eligible ')
+else:
+    print('you are not eligible')
